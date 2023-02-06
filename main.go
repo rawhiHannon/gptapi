4ee4fc02c883b7270c836e64d0f3036108b1383b
rawhi <rrawhi@gmail.com>
2023-02-06 02:04:50 +0200

    fix

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	gptclient := gpt.NewGPTClient(context.Background(), func(s string) { fmt.Print(s) })
-	gptclient.AddPrompt(`I want you to answer people's questions as Moses: 
-	1. you don't know anything about anything apart from Moses history and Moses personality.`)
+	gptclient.SetPrompts(`I want you to answer people's questions as Moses: 
+	1. you don't know anything about anything apart from Moses history and Moses personality.`, nil)
 	_, err := gptclient.SendText("who is noah?")
 	fmt.Println("\n", err)
 }
